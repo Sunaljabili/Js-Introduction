e97123e2549263716f1702398b0f4632ea6786f0
Sunaljabili <you@example.com>
2021-10-25 19:19:32 +0400

    Js intro

---
+++ JS/script.js
@@ -0,0 +1,89 @@
+// document.write("JS intro");
+// console.log("Here is log");
+
+// "use strict";
+// x = 10;
+// console.log(x);
+// firstname = "Sunal"
+// console.log(firstname)
+
+// var,let,const
+// var age = 18;
+// age =20;
+// console.log(age)
+
+// var number =10n;
+// console.log();
+
+// firstname = "Sunal";
+// console.log(firstname);
+
+// var firstname = "Sunal";
+// firstname = "Javid";
+
+// var firstname = "Emil"
+// console.log(firstname);
+
+
+// let firstname ="Sunal";
+// firstname="Javid"
+// let firstname ="Emil"
+//  console.log(firstname);
+
+// let pi =3.14;
+// pi=4;
+// console.log(pi);
+
+// var isMarried =true;
+// var isMarried =false;
+// console.log(isMarried);
+
+// let age;
+// console.log(age);
+
+// let number2=7;
+
+// Riyazi Operatorlar
+// console.log(number1 + number2);
+// console.log(number1-number2);
+// console.log(number1*number2);
+// console.log(number1**number2);  // 2*2*2
+// console.log(number1/number2);
+// console.log(number1 %number2);
+// number1++;
+// console.log(--number1);
+
+// Menimsetme Operatorlari
+
+// number1+=5; 
+// number1 = number1+5;
+// number1-=5;
+// number1/=2
+
+
+// Muqasiye Operatorlari
+
+// let num1 =10;  //number
+// let num2 ="10" //string
+
+// let num1 ="5";  //number
+// let num2 ="5" //string
+
+
+// console.log(6 <= 5);
+
+
+// And Operator
+// console.log(5>6 && 7>5 && 3>4);
+
+// OR Operator
+// console.log(5>6 || 7>5);
+
+// console.log(! 6>4);
+
+
+
+let num1 =5;
+let num2=10;
+
+console.log(num1 == num2);
